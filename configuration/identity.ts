856e75dbd94ed5d9f1a3cb62b9b0742cb6ab0ee5
Mattvand14 <mattvand@ad.unc.edu>
2025-03-01 14:39:53 -0500

    indentity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Matthew VanDeusen`;
 export const OWNER_DESCRIPTION: string = `CS Student Athlete at UNC `;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `MIC`;
+export const AI_TONE: string = `Helpful`;
+export const AI_ROLE: string = `Music Industry Navigation Assistant`;
