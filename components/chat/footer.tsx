4dbfaa1759ea5d0323ce0dc0662e60ff6825900b
Mattvand14 <mattvand@ad.unc.edu>
2025-03-01 14:17:03 -0500

    white text

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 export default function ChatFooter() {
   return (
-    <div className="w-full text-xs flex pt-2 text-gray-500">
+    <div className="w-full text-xs flex pt-2 text-gray-500 bg-transparent">
       <div className="flex-grow text-left">
         {/* Left Pane */}
-        <Link href="/terms" className="hover:underline">
+        <Link href="/terms" className="hover:underline text-white">
           Terms of Service
         </Link>
       </div>
-      <div className="flex-grow text-center">
+      <div className="flex-grow text-center text-white">
         {/* Center Pane */}
         {FOOTER_MESSAGE}
       </div>
-      <div className="flex-grow text-right">
+      <div className="flex-grow text-right text-white">
         {/* Right Pane */}
         {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
         <a
